6ec1da86a75c204917da9268b918bf3ffb866af9
Nehar Jashari <nehar@starlabspro.com>
2020-04-20 21:10:00 +0200

    Adding the other components from the design

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Navbar from "./Navbar"
-import Footer from "./Footer"
+import Footer from "./Footer/Footer"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "font-awesome/css/font-awesome.min.css"
 import "../css/templatemo-style.css"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
 import Header from "../components/Header"
+import About from "../components/About/About"
+import Blog from "../components/Blog/Blog"
+import OurWork from "../components/OurWork/OurWork"
+import Contact from "../components/Contact/Contact"
 
 export default () => (
   <Layout>
     <Header />
+    {/* <About /> */}
+    {/* <Blog /> */}
+    {/* <OurWork /> */}
+    <Contact />
   </Layout>
 )
